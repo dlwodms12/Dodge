cb45a4e6d3fc59fc3e63ae3f74153552d255281a
Jae Eun <pdw05498@naver.com>
2026-05-10 06:30:15 +0900

    총알의 트리거 이벤트 구현
    OnTriggerEnter 메소드 추가 및 충돌한 상대의 컴포넌트를 가져와 Die 메소드를 실행하는 기능 추가

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -14,4 +14,22 @@ public class Bullet : MonoBehaviour
         //3초 뒤 파괴
         Destroy(gameObject, 3f);
     }
+
+    //Is Trigger 가 체크된 트리거 콜라이더이므로 OnTriggerEnter()를 작성
+    private void OnTriggerEnter(Collider other)
+    {
+        //충돌한 오브젝트가 player라면
+        if(other.tag == "Player")
+        {
+            //충돌한 오브젝트에서 playerController 컴포넌트 가져오기
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            //컴포넌트를 가져오는데 성공했다면
+            if (playerController != null)
+            {
+                //상대방 컴포넌트의 Die() 메서드 실행
+                playerController.Die();
+            }
+        }
+    }
 }
